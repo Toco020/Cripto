41b4e5a4e209323a9a870d9dc3f590b7d8226a8a
Toco020 <joseisoppo016@gmail.com>
2023-03-17 16:57:02 -0300

    modificações de eficiência por escala

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 // 1.000.000.000.000.000
+//    99.999.820.000.081
+// 9.754.391.154.146.016.630.121 = 98.764.321.261 ** 2
 
 function buscaPrimo(pos) {
     let divisivel = 0, count = 0, primo = 0;
@@ -20,24 +22,19 @@ function produto(num1, num2){
 }
 
 function doisTermosProduto(produto){
-    let teste = 0, p1 = 1, p2;
+    let p1 = 1, p2;
 
-    while (verificaPrimo(p2) == false) {
-        teste++;
+    while (!verificaPrimo(p2)) {
         p1 = proximoPrimo(p1);
         if (Number.isInteger(produto/p1)) p2 = produto/p1;
-
-        // console.log(p1);
-        // console.log(p2);
-        // console.log(Number.isInteger(produto/p1));
     }
 
     return [p1, p2];
 }
 
 function proximoPrimo(prant){
-
     let divisivel = 0;
+
     while (true){
         prant++;
         for (let i = 1; i <= prant; i++) {
@@ -52,14 +49,19 @@ function proximoPrimo(prant){
 }
 
 function verificaPrimo(primo){
-    if (primo != false){
-        let divisivel = 0;
-        for (let i = 1; i <= primo; i++) {
-            if (primo % i === 0) {
-                divisivel++;
-            }
+    let divisivel = 0;
+    
+    for (let i = 1; i <= primo; i++) {
+        if (primo % i === 0) {
+            divisivel++;
         }
-        return divisivel == 2;
+        // console.log(primo);
     }
+    return divisivel == 2;
+}
+
+function escalaPrimo(escala, sentido){
+    while (!verificaPrimo(escala)) escala += sentido;
+    return escala;
 }
 
